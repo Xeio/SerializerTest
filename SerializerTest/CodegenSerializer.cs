4ec59ea7c16f95ac2d2466fe2df04b1f41ab8940
Joshua Shaffer <xeio@live.com>
2020-01-25 00:39:24 -0500

    Codegen for IEnumerable writing properties directly

--- SerializerTest/CodegenSerializer.cs
+++ SerializerTest/CodegenSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -22,24 +23,20 @@ namespace SerializerTest
         private static Delegate BuildCache<T>()
         {
             var type = typeof(T);
-            if (type.IsValueType)
-            { throw new NotImplementedException(); }
-            var members = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
-
             var enumerableType = type.GetInterfaces().FirstOrDefault(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>));
-            if(enumerableType != null)
+            if (enumerableType != null)
             {
                 var enumerableGenericType = enumerableType.GetGenericArguments()[0];
                 var delegateType = typeof(Action<,,>).MakeGenericType(enumerableType, typeof(Utf8JsonWriter), typeof(string));
-                if(enumerableGenericType == typeof(string))
+                if (enumerableGenericType == typeof(string))
                 {
                     return (Action<IEnumerable<string>, Utf8JsonWriter, string>)WriteEnumerableString;
                 }
-                if(NumericEnumerableWriters.NumericEnumerableDelegates.TryGetValue(enumerableGenericType, out var del))
+                if (NumericEnumerableWriters.NumericEnumerableDelegates.TryGetValue(enumerableGenericType, out var del))
                 {
                     return del;
                 }
-                return EnumerableMethod.MakeGenericMethod(enumerableGenericType).CreateDelegate(delegateType);
+                return WriteEnumerable(enumerableGenericType);
             }
 
             var objectParam = Expression.Parameter(type, "object");
@@ -53,39 +50,50 @@ namespace SerializerTest
                     Expression.Call(writerParam, "WriteStartObject", null),
                     Expression.Call(writerParam, "WriteStartObject", null, nameParam))
             };
-            foreach (PropertyInfo property in members)
-            {
-                if(GetPrimitiveWriterExpression(property, objectParam, writerParam, out var writePrimitiveExpression))
-                {
-                    writeObjectBlockContents.Add(writePrimitiveExpression);
-                }
-                //TODO: Structs?
-                else
-                {
-                    writeObjectBlockContents.Add(
-                        Expression.IfThenElse(
-                            Expression.Equal(Expression.Property(objectParam, property), Expression.Constant(null)),
-                            Expression.Call(writerParam, "WriteNull", null, Expression.Constant(property.Name)),
-                            Expression.Invoke(Expression.Call(typeof(CodegenSerializer), nameof(GetOrPopulateCacheDelegate), new[] { property.PropertyType }), Expression.Property(objectParam, property), writerParam, Expression.Constant(property.Name))));
-                }
-            }
+            writeObjectBlockContents.AddRange(BuildPropertyWriters(type, objectParam, writerParam));
             writeObjectBlockContents.Add(Expression.Call(writerParam, "WriteEndObject", null));
 
             var fullMethodBlock = Expression.Block(writeObjectBlockContents);
 
             var lambda = Expression.Lambda(fullMethodBlock, objectParam, writerParam, nameParam);
-            return lambda.Compile(false);
+            return lambda.Compile();
         }
 
-        private static void WriteEnumerable<T>(IEnumerable<T> objects, Utf8JsonWriter writer, string _)
+        private static Delegate WriteEnumerable(Type enumerableGenericType)
         {
-            var method = GetOrPopulateCacheDelegate<T>();
-            writer.WriteStartArray();
-            foreach (var obj in objects)
+            var objectParam = Expression.Parameter(typeof(IEnumerable<>).MakeGenericType(enumerableGenericType), "object");
+            var writerParam = Expression.Parameter(typeof(Utf8JsonWriter), "writer");
+            var nameParam = Expression.Parameter(typeof(string), "name");
+
+            var enumerator = Expression.Variable(typeof(IEnumerator<>).MakeGenericType(enumerableGenericType));
+            var breakLabel = Expression.Label("objectsLoopBreak");
+            var obj = Expression.Variable(enumerableGenericType);
+
+            var writeObjectBlockContents = new List<Expression>
             {
-                method(obj, writer, null);
-            }
-            writer.WriteEndArray();
+                Expression.Assign(obj, Expression.Property(enumerator, "Current")),
+                Expression.Call(writerParam, "WriteStartObject", null)
+            };
+            writeObjectBlockContents.AddRange(BuildPropertyWriters(enumerableGenericType, obj, writerParam));
+            writeObjectBlockContents.Add(Expression.Call(writerParam, "WriteEndObject", null));
+
+            var objectsLoopBody = Expression.IfThenElse(
+                                    Expression.IsTrue(Expression.Call(enumerator, typeof(IEnumerator).GetMethod("MoveNext"))),
+                                    Expression.Block(writeObjectBlockContents),
+                                    Expression.Goto(breakLabel)
+                                    );
+
+            var fullMethodBlock = Expression.Block(
+                    new[] { enumerator, obj },
+                    Expression.Call(writerParam, "WriteStartArray", null),
+                    Expression.Assign(enumerator, Expression.Call(objectParam, "GetEnumerator", null)),
+                    Expression.Loop(objectsLoopBody, breakLabel),
+                    Expression.Call(writerParam, "WriteEndArray", null),
+                    Expression.Call(enumerator, typeof(IDisposable).GetMethod("Dispose"))
+                );
+
+            var lambda = Expression.Lambda(fullMethodBlock, objectParam, writerParam, nameParam);
+            return lambda.Compile();
         }
 
         private static Action<T, Utf8JsonWriter, string> GetOrPopulateCacheDelegate<T>()
@@ -108,13 +116,35 @@ namespace SerializerTest
             writer.WriteEndArray();
         }
 
+        private static IEnumerable<Expression> BuildPropertyWriters(Type type, Expression objectParam, Expression writerParam)
+        {
+            var members = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+
+            foreach (PropertyInfo property in members)
+            {
+                if (GetPrimitiveWriterExpression(property, objectParam, writerParam, out var writePrimitiveExpression))
+                {
+                    yield return writePrimitiveExpression;
+                }
+                else
+                {
+                    yield return
+                        Expression.IfThenElse(
+                            Expression.Equal(Expression.Property(objectParam, property), Expression.Constant(null)),
+                            Expression.Call(writerParam, "WriteNull", null, Expression.Constant(property.Name)),
+                            Expression.Invoke(Expression.Call(typeof(CodegenSerializer), nameof(GetOrPopulateCacheDelegate), new[] { property.PropertyType }), Expression.Property(objectParam, property), writerParam, Expression.Constant(property.Name)));
+                }
+
+            }
+        }
+
         private static bool GetPrimitiveWriterExpression(PropertyInfo property, Expression objectParam, Expression writerParam, out Expression expression)
         {
             expression = null;
             Expression propertyExpression = Expression.Property(objectParam, property);
             var propertyType = property.PropertyType;
             var underlyingType = Nullable.GetUnderlyingType(property.PropertyType);
-            if(underlyingType != null)
+            if (underlyingType != null)
             {
                 propertyType = underlyingType;
                 //Have to cast nullable types to non-null so the method parameters match (we'll add the check for null at the end)
@@ -146,7 +176,7 @@ namespace SerializerTest
                         Expression.Constant(property.Name), Expression.Convert(Expression.Property(objectParam, property), typeof(int)));
             }
 
-            if(underlyingType != null && expression != null)
+            if (underlyingType != null && expression != null)
             {
                 //If we're a nullable, wrap the whole thing in a null check
                 expression = Expression.IfThenElse(
