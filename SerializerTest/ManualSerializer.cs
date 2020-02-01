de8d3feb0b30bff67a43027281f06575ec2ac1a8
Joshua Shaffer <xeio@live.com>
2020-02-01 13:46:23 -0500

    Move Json encode out of loop in ManualSerializer

--- SerializerTest/ManualSerializer.cs
+++ SerializerTest/ManualSerializer.cs
@@ -6,18 +6,20 @@ namespace SerializerTest
 {
     public static class ManualSerializer
     {
+        static readonly JsonEncodedText _fooStringName = JsonEncodedText.Encode("FooString");
+        static readonly JsonEncodedText _barDecimalName = JsonEncodedText.Encode("BarDecimal");
+        static readonly JsonEncodedText _bazIntName = JsonEncodedText.Encode("BazInt");
+
         public static void Serialize(List<TestObj> objects, Utf8JsonWriter writer)
         {
             writer.WriteStartArray();
-            var fooStringName = JsonEncodedText.Encode("FooString");
-            var barDecimalName = JsonEncodedText.Encode("BarDecimal");
-            var bazIntName = JsonEncodedText.Encode("BazInt");
+            
             foreach (var obj in objects)
             {
                 writer.WriteStartObject();
-                writer.WriteString(fooStringName, obj.FooString);
-                writer.WriteNumber(barDecimalName, obj.BarDecimal);
-                writer.WriteNumber(bazIntName, obj.BazInt);
+                writer.WriteString(_fooStringName, obj.FooString);
+                writer.WriteNumber(_barDecimalName, obj.BarDecimal);
+                writer.WriteNumber(_bazIntName, obj.BazInt);
                 writer.WriteEndObject();
             }
             writer.WriteEndArray();
